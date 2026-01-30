74ca7553678e24df6ff44694a4b0b22c447a6e78
gabriel01913 <95130441+gabriel01913@users.noreply.github.com>
2026-01-30 15:36:01 -0300

    fix html and vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://gabriel01913.github.io/OPTCG-Proxy-Generator/',
+  base: '/OPTCG-Proxy-Generator/',
 })
