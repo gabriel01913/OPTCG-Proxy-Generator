44a914b23e47950d9fd520fec9a1ebf4e1ba4536
gabriel01913 <95130441+gabriel01913@users.noreply.github.com>
2026-01-30 16:27:53 -0300

    Proxy card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import card from '../assets/OP01-001.png'
+import card from '../assets/proxy.png'
 
 function Card() {
   return <img className="h-auto w-full aspect[600/838] rounded-lg" src={card}></img>
