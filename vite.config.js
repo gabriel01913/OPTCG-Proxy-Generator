280bce7b18e56f5985201f64b8db39a129ed50dc
gabriel01913 <95130441+gabriel01913@users.noreply.github.com>
2026-01-31 12:41:24 -0300

    add sharp to compress files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSharp } from 'vite-plugin-sharp'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteSharp({
+      presets: {
+        optimized: {
+          width: 800,
+          format: 'webp',
+          quality: 75,
+        },
+      },
+    }),
+  ],
   base: '/OPTCG-Proxy-Generator/',
 })
